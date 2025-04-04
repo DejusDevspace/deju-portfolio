ee518e37ab12f1d5997214c63744a06711b1ade9
Deju Adejo <ojomideju2003@gmail.com>
2025-04-04 21:27:42 +0100

    modified about section

--- src/components/aboutSection/AboutMain.jsx
+++ src/components/aboutSection/AboutMain.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import SectionHeading from "../common/SectionHeading";
+import AboutImage from "./AboutImage";
 
 function AboutMain() {
   return (
     <div className="container mx-auto min-h-screen my-8 xl:my-12">
-      <div className="flex flex-col items-center justify-center text-center">
-        <div>
-          <h1>AboutMain</h1>
+      <div className="flex flex-col items-center justify-center text-center px-6 xl:px-0">
+        <div className="flex flex-col gap-8 mb-8">
+          <SectionHeading text="About">
+            <span className="text-accent">Me</span>
+          </SectionHeading>
+          <p className="max-w-[500px] xl:max-w-[800px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            officia exercitationem, quos facere veritatis, quo rem cumque
+            molestias, delectus nobis aut deleniti cum? Minus nam minima commodi
+            doloremque fuga expedita.
+          </p>
+        </div>
+        <div className="flex flex-col gap-8 justify-center items-center xl:flex-row w-full xl:justify-around py-8">
+          <AboutImage />
+          <div>Content</div>
         </div>
       </div>
     </div>
