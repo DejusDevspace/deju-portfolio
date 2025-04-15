96ad61ae73899e9223ced8c60bc002bc3259d205
Deju Adejo <ojomideju2003@gmail.com>
2025-04-15 10:51:42 +0100

    added boilerplate component code

---
+++ src/components/projectSection/ProjectItem.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { GoArrowUpRight } from "react-icons/go";
+import { FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+function ProjectItem({ project }) {
+  const isNotEven = project.id % 2 !== 0;
+  const textSlideFrom = isNotEven ? 100 : -100;
+  const imageSlideFrom = isNotEven ? -100 : 100;
+
+  return (
+    <motion.div
+      className={`flex flex-col p-4 gap-6 xl:flex-row xl:gap-[30px] ${
+        isNotEven ? "xl:flex-row-reverse" : ""
+      }`}
+      whileHover={{ scale: 1.02 }}
+    >
+      {/* Text Section */}
+      <motion.div
+        className="w-full xl:w-[50%] flex flex-col gap-2 xl:justify-between order-2 xl:order-none"
+        initial={{ opacity: 0, x: textSlideFrom }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        viewport={{ once: true, amount: 0.2 }}
+      >
+        <div className="text-8xl leading-none font-extrabold text-accent text-outline">
+          {project.num}
+        </div>
+        <h2 className="font-bold leading-none text-4xl text-primary group-hover:text-accent transition-all duration-500 capitalize">
+          {project.name}
+        </h2>
+        <p className="text-primary/50">{project.description}</p>
+        <ul className="flex gap-4 flex-wrap">
+          {project.stack.map((item, index) => (
+            <li key={index} className="text-sm xl:text-md text-accent">
+              {item.name}
+              {index !== project.stack.length - 1 && ","}
+            </li>
+          ))}
+        </ul>
+
+        <div className="border border-primary/20" />
+
+        <div className="flex gap-4">
+          <div className="flex justify-center items-center group w-[60px] h-[60px] bg-secondary rounded-full cursor-pointer">
+            <a
+              href={project.links.live}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GoArrowUpRight className="text-2xl group-hover:text-accent" />
+            </a>
+          </div>
+          <div className="flex justify-center items-center group w-[60px] h-[60px] bg-secondary rounded-full cursor-pointer">
+            <a
+              href={project.links.github}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="text-2xl group-hover:text-accent" />
+            </a>
+          </div>
+        </div>
+      </motion.div>
+
+      {/* Image Section */}
+      <motion.div
+        className="w-full xl:w-[50%]"
+        initial={{ opacity: 0, x: imageSlideFrom }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
+        viewport={{ once: true, amount: 0.2 }}
+      >
+        <div
+          className="relative group flex justify-center items-center bg-accent/10 rounded-lg shadow-md hover:shadow-accent transition-all duration-300
+                        h-[250px] sm:h-[300px] md:h-[350px] xl:h-[460px] overflow-hidden"
+        >
+          <img
+            src={project.image}
+            alt={project.name}
+            className="object-cover object-center w-full h-full rounded-lg z-10"
+            loading="lazy"
+          />
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+}
+
+export default ProjectItem;
--- src/components/projectSection/ProjectMain.jsx
+++ src/components/projectSection/ProjectMain.jsx
@@ -1,12 +1,67 @@
-import React from "react";
-import SectionHeading from "../common/SectionHeading";
+import React, { useState } from "react";
+import { projects } from "./sectionData";
+import ProjectItem from "./ProjectItem";
+import { motion } from "framer-motion";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
 function ProjectMain() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const totalProjects = projects.length;
+
+  function moveNext() {
+    setCurrentIndex((prev) => prev + 1);
+  }
+
+  function movePrev() {
+    setCurrentIndex((prev) => prev - 1);
+  }
+
   return (
-    <div className="container mx-auto my-12 xl:my-16">
-      <div className="flex flex-col justify-center items-center">
-        <div>ProjectMain</div>
-      </div>
+    <div className="relative flex justify-center items-center py-12 xl:py-24 overflow-hidden">
+      <motion.div
+        className="container mx-auto relative z-10 flex flex-col gap-12 xl:gap-24"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        viewport={{ once: false, amount: 0.2 }}
+      >
+        <motion.div
+          initial={{ opacity: 0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            duration: 0.7,
+            ease: "easeOut",
+          }}
+          viewport={{ once: false, amount: 0.1 }}
+        >
+          <ProjectItem project={projects[currentIndex]} />
+        </motion.div>
+
+        <div className="flex gap-4 w-full justify-center">
+          <button
+            onClick={movePrev}
+            disabled={currentIndex === 0}
+            className={`flex justify-center items-center group w-[50px] h-[50px] ${
+              currentIndex === 0
+                ? "bg-accent/50 cursor-not-allowed"
+                : "bg-accent hover:bg-neonMagenta hover:shadow-[0_0_25px_#9F00FF] cursor-pointer"
+            } cursor-pointer rounded-md`}
+          >
+            <IoIosArrowBack className="text-3xl group-hover:text-secondary" />
+          </button>
+          <button
+            onClick={moveNext}
+            disabled={currentIndex === totalProjects - 1}
+            className={`flex justify-center items-center group w-[50px] h-[50px] ${
+              currentIndex === totalProjects - 1
+                ? "bg-accent/50 cursor-not-allowed"
+                : "bg-accent hover:bg-neonMagenta hover:shadow-[0_0_25px_#9F00FF] cursor-pointer"
+            } bg-accent cursor-pointer rounded-md`}
+          >
+            <IoIosArrowForward className="text-3xl group-hover:text-secondary" />
+          </button>
+        </div>
+      </motion.div>
     </div>
   );
 }
