a6e6b6db96928644971a1cb352f03e5c1d92b65b
Deju Adejo <ojomideju2003@gmail.com>
2025-04-19 16:26:27 +0100

    installed react-three packages

--- src/components/contactSection/ContactMain.jsx
+++ src/components/contactSection/ContactMain.jsx
@@ -5,6 +5,7 @@ function ContactMain() {
   return (
     <div className="container mx-auto flex flex-col items-center py-12 xl:py-24">
       <SectionHeading text="Contact Me" />
+      <div className="my-12 bg-white/5 backdrop-blur-lg rounded-2xl border border-white/10 p-6 shadow-[0_0_20px_5px_var(--color-neonBlue)]"></div>
     </div>
   );
 }
