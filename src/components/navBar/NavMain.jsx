63a5f713e4b39b40fcbf13720f2eca2e59d7af0f
Deju Adejo <ojomideju2003@gmail.com>
2025-04-20 21:29:32 +0100

    added navbar menu selection functionality

--- src/components/navBar/NavMain.jsx
+++ src/components/navBar/NavMain.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import DesktopNav from "./DesktopNav";
 import MobileNav from "./MobileNav";
 import navList from "./navList";
 
 function NavMain() {
+  const [isSelected, setSelected] = useState(0);
+
+  const handleSelect = (id) => {
+    setSelected(id);
+  };
+
   return (
     <header className="fixed bg-gray-900 w-full py-4 z-20">
       <div className="container mx-auto flex justify-between items-center px-6 xl:px-4">
@@ -14,7 +20,11 @@ function NavMain() {
           Deju
         </h1>
         <div className="hidden lg:flex">
-          <DesktopNav data={navList} />
+          <DesktopNav
+            data={navList}
+            active={isSelected}
+            onClick={handleSelect}
+          />
         </div>
         <div className="lg:hidden">
           <MobileNav data={navList} />
