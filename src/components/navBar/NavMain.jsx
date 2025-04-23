94ef958e74851eef6a238f7b248068d1a9ecfced
Deju Adejo <ojomideju2003@gmail.com>
2025-04-23 22:30:33 +0100

    removed gradient text on header

--- src/components/navBar/NavMain.jsx
+++ src/components/navBar/NavMain.jsx
@@ -13,12 +13,7 @@ function NavMain() {
   return (
     <header className="fixed bg-gray-900 w-full py-4 z-20">
       <div className="container mx-auto flex justify-between items-center px-6 xl:px-4">
-        <h1
-          className="text-4xl font-bold bg-gradient-to-r from-cyan-500 via-purple-500 to-pink-500
-        bg-clip-text text-transparent"
-        >
-          Deju
-        </h1>
+        <h1 className="text-2xl font-bold">Deju</h1>
         <div className="hidden lg:flex">
           <DesktopNav
             data={navList}
