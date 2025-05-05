7beca094140588469ebefc685643cf58d4666aa2
Deju Adejo <ojomideju2003@gmail.com>
2025-05-05 02:28:47 +0100

    added first version typewriter for hero section

--- src/components/heroSection/HeroMain.jsx
+++ src/components/heroSection/HeroMain.jsx
@@ -1,26 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SectionHeading from "../common/SectionHeading";
 import DownloadCVButton from "../common/DownloadCVButton";
 import HeroBackgroundScene from "./HeroBackgroundScene";
 
 function HeroMain() {
+  const fullText = "Hi, I'm Deju.";
+  const [displayedText, setDisplayedText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < fullText.length) {
+      const timeout = setTimeout(() => {
+        setDisplayedText((prev) => prev + fullText[index]);
+        setIndex(index + 1);
+      }, 200);
+      return () => clearTimeout(timeout);
+    }
+  }, [index]);
+
   return (
     <div className="relative flex items-center justify-center min-h-screen w-full">
-      <HeroBackgroundScene />
+      {/* <HeroBackgroundScene /> */}
       <div className="relative flex flex-col text-center items-center justify-center text-white">
-        <SectionHeading text="Hi, I'm Deju" />
-        <p className="text-md xl:text-lg mt-4">
-          Full Stack Data Scientist | Generative AI | MLOps | Backend Developer{" "}
+        <SectionHeading
+          text={
+            <>
+              {displayedText}
+              <span className="text-accent animate-blink">|</span>
+            </>
+          }
+        />
+        <p className="text-md xl:text-lg max-w-[500px] px-2 xl:max-w-[800px] my-4">
+          I am a final year Mechatronics Engineering student at Bowen
+          University. I love doing one thing or the other with code. I would
+          fill this section when I have clearer ideas
         </p>
         <DownloadCVButton>
-          {/* Hire Me Button */}
           <a
             href="https://www.linkedin.com/in/deju-adejo/"
             target="_blank"
             rel="noopener noreferrer"
             className="hidden xl:block bg-cyan-400 text-black font-bold py-3 px-6 rounded-lg cursor-pointer 
-            shadow-[0_0_15px_#00FFFF] hover:bg-purple-600 hover:text-white hover:shadow-[0_0_25px_#9F00FF] 
-            transition-all duration-300"
+              shadow-[0_0_15px_#00FFFF] hover:bg-purple-600 hover:text-white hover:shadow-[0_0_25px_#9F00FF] 
+              transition-all duration-300"
           >
             Hire Me
           </a>
