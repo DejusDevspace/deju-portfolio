8f1a725b053a86cede9433b3eee14975047aa507
Deju Adejo <ojomideju2003@gmail.com>
2025-05-05 02:58:32 +0100

    fixed typo in text

--- src/components/common/SectionHeading.jsx
+++ src/components/common/SectionHeading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SectionHeading({ text, children }) {
   return (
-    <h1 className="text-3xl xl:ext-5xl font-bold leading-tight">
+    <h1 className="text-3xl xl:text-5xl font-bold leading-tight">
       {text} {children}
     </h1>
   );
