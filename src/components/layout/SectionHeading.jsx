27ead9a2881f3c484d7b7b7043d7bf1c92b59d16
Deju Adejo <ojomideju2003@gmail.com>
2025-05-30 16:00:30 +0100

    refactor: changed section heading design

--- src/components/layout/SectionHeading.jsx
+++ src/components/layout/SectionHeading.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const SectionHeading = ({ text, children }) => {
   return (
     <div className="flex items-center mb-8">
-      <span className="text-3xl xl:text-4xl text-accent-primary mr-4">
-        /{text} {children}
+      <div className="flex-grow border-b border-gray-600"></div>
+      <span className="text-3xl xl:text-4xl text-accent-primary m-4">
+        {text} {children}
       </span>
       <div className="flex-grow border-b border-gray-600"></div>
     </div>
