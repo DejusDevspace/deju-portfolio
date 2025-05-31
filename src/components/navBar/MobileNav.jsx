899bb63b7b4fc6fd32eee17ffbd1b88e4123319b
Deju Adejo <ojomideju2003@gmail.com>
2025-05-31 20:18:11 +0100

    added navbar header animations

--- src/components/navBar/MobileNav.jsx
+++ src/components/navBar/MobileNav.jsx
@@ -60,7 +60,6 @@ const MobileNav = ({ data, active, onClick }) => {
     <motion.div
       initial={{ opacity: 0, x: 100 }}
       animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: 100 }}
       transition={{ duration: 0.6 }}
       className="relative"
       ref={menuRef}
--- src/components/navBar/NavMain.jsx
+++ src/components/navBar/NavMain.jsx
@@ -53,7 +53,6 @@ const NavMain = () => {
           className="text-xl xl:text-2xl font-bold"
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -100 }}
           transition={{ duration: 0.6, ease: "easeInOut" }}
           viewport={{ once: true }}
         >
