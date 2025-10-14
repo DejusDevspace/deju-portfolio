9a678c8bff3203d0ba024cb72b842246da3c5e30
Deju Adejo <ojomideju2003@gmail.com>
2025-10-14 11:10:56 +0100

    refactor resume path

--- src/components/common/DownloadCVButton.jsx
+++ src/components/common/DownloadCVButton.jsx
@@ -5,7 +5,7 @@ const DownloadCVButton = ({ children }) => {
   return (
     <div className="flex flex-col xl:flex-row items-center gap-8 mt-4">
       <a
-        href="/Ojomideju_Adejo_CV.pdf"
+        href="/Ojomideju_Adejo_Resume.pdf"
         download
         className="flex items-center gap-2 bg-black text-accent/90 px-6 py-3 rounded-lg cursor-pointer
               shadow-[var(--shadow-neon)] hover:scale-105 transition-all duration-300"
