91d6dbab55d45b7efff3cb84a499b80e607e5edc
Deju Adejo <ojomideju2003@gmail.com>
2026-05-16 22:51:29 +0100

    feat: add blog url to nav

--- src/components/navBar/MobileNav.jsx
+++ src/components/navBar/MobileNav.jsx
@@ -2,86 +2,91 @@ import React, { useState, useRef, useEffect } from "react";
 import { CiMenuFries } from "react-icons/ci";
 import { motion, AnimatePresence } from "framer-motion";
 import SocialIcon from "../common/SocialIcon";
+import { BLOG_URL } from "./navData";
 
 const MobileNav = ({ navList, socialList, active, onClick, ThemeToggle }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef();
+	const [isOpen, setIsOpen] = useState(false);
+	const menuRef = useRef();
 
-  const toggleMenu = () => {
-    setIsOpen((prev) => !prev);
-  };
+	const toggleMenu = () => {
+		setIsOpen((prev) => !prev);
+	};
 
-  const handleOutsideClick = (e) => {
-    if (menuRef.current && !menuRef.current.contains(e.target)) {
-      setIsOpen(false);
-    }
-  };
+	const handleOutsideClick = (e) => {
+		if (menuRef.current && !menuRef.current.contains(e.target)) {
+			setIsOpen(false);
+		}
+	};
 
-  useEffect(() => {
-    if (isOpen) {
-      document.addEventListener("mousedown", handleOutsideClick);
-    } else {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    }
+	useEffect(() => {
+		if (isOpen) {
+			document.addEventListener("mousedown", handleOutsideClick);
+		} else {
+			document.removeEventListener("mousedown", handleOutsideClick);
+		}
 
-    return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    };
-  }, [isOpen]);
+		return () => {
+			document.removeEventListener("mousedown", handleOutsideClick);
+		};
+	}, [isOpen]);
 
-  return (
-    <motion.div
-      initial={{ opacity: 0, x: 100 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.6 }}
-      className="relative"
-      ref={menuRef}
-    >
-      <div className="flex items-center gap-4">
-        {socialList.map((social, index) => (
-          <SocialIcon key={index} icon={social.icon} link={social.link} />
-        ))}
-        {ThemeToggle}
-      </div>
+	return (
+		<motion.div
+			initial={{ opacity: 0, x: 100 }}
+			animate={{ opacity: 1, x: 0 }}
+			transition={{ duration: 0.6 }}
+			className="relative"
+			ref={menuRef}
+		>
+			<div className="flex items-center gap-4">
+				{socialList.map((social, index) => (
+					<SocialIcon key={index} icon={social.icon} link={social.link} />
+				))}
+				{ThemeToggle}
+			</div>
 
-      <div className="fixed bottom-0 right-0 p-4 m-4 z-20 rounded-xl bg-secondary shadow-xl">
-        <CiMenuFries
-          className="text-[32px] cursor-pointer"
-          onClick={toggleMenu}
-        />
-        <AnimatePresence>
-          {isOpen && (
-            <motion.ul
-              initial={{ opacity: 0, y: -10 }}
-              animate={{ opacity: 1, y: -250 }}
-              exit={{ opacity: 0, y: -10 }}
-              className="absolute right-0 -mt-6 bg-secondary rounded-lg shadow-xl
+			<div className="fixed bottom-0 right-0 p-4 m-4 z-20 rounded-xl bg-secondary shadow-xl">
+				<CiMenuFries
+					className="text-[32px] cursor-pointer"
+					onClick={toggleMenu}
+				/>
+				<AnimatePresence>
+					{isOpen && (
+						<motion.ul
+							initial={{ opacity: 0, y: -10 }}
+							animate={{ opacity: 1, y: -250 }}
+							exit={{ opacity: 0, y: -10 }}
+							className="absolute right-0 -mt-6 bg-secondary rounded-lg shadow-xl
             py-4 px-6 flex flex-col gap-4 z-20"
-            >
-              {navList.map((link, index) => (
-                <li
-                  key={index}
-                  onClick={() => {
-                    toggleMenu();
-                    onClick(index);
-                  }}
-                >
-                  <a
-                    href={`#${link.toLowerCase()}`}
-                    className={`${
-                      active === index ? "text-accent" : "text-primary"
-                    } hover:text-accent/70 cursor-pointer capitalize`}
-                  >
-                    {link}
-                  </a>
-                </li>
-              ))}
-            </motion.ul>
-          )}
-        </AnimatePresence>
-      </div>
-    </motion.div>
-  );
+						>
+							{navList.map((link, index) => (
+								<li
+									key={index}
+									onClick={() => {
+										toggleMenu();
+										onClick(index);
+									}}
+								>
+									<a
+										href={
+											item === "Blog" ? `${BLOG_URL}` : `#${item.toLowerCase()}`
+										}
+										target={item === "Blog" ? "_blank" : undefined}
+										rel={item === "Blog" ? "noopener noreferrer" : undefined}
+										className={`${
+											active === index ? "text-accent" : "text-primary"
+										} hover:text-accent/70 cursor-pointer capitalize`}
+									>
+										{link}
+									</a>
+								</li>
+							))}
+						</motion.ul>
+					)}
+				</AnimatePresence>
+			</div>
+		</motion.div>
+	);
 };
 
 export default MobileNav;
--- src/components/navBar/navData.js
+++ src/components/navBar/navData.js
@@ -3,13 +3,21 @@ import { RiTwitterXFill } from "react-icons/ri";
 import { FaGithub } from "react-icons/fa";
 import { MdOutgoingMail } from "react-icons/md";
 
-export const navList = ["Home", "About", "Experience", "Projects", "Contact"];
+export const BLOG_URL = "https://blog.dejuadejo.vercel.app";
+export const navList = [
+	"Home",
+	"About",
+	"Experience",
+	"Projects",
+	"Blog",
+	"Contact",
+];
 export const socialLinks = [
-  {
-    icon: TbBrandLinkedinFilled,
-    link: "https://www.linkedin.com/in/deju-adejo/",
-  },
-  { icon: FaGithub, link: "https://github.com/DejusDevspace" },
-  { icon: RiTwitterXFill, link: "https://x.com/adejo_deju" },
-  { icon: MdOutgoingMail, link: "mailto:ojomideju2003@gmail.com" },
+	{
+		icon: TbBrandLinkedinFilled,
+		link: "https://www.linkedin.com/in/deju-adejo/",
+	},
+	{ icon: FaGithub, link: "https://github.com/DejusDevspace" },
+	{ icon: RiTwitterXFill, link: "https://x.com/adejo_deju" },
+	{ icon: MdOutgoingMail, link: "mailto:ojomideju2003@gmail.com" },
 ];
